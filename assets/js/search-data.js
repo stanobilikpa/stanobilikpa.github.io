427f953b3e0849a1379b9233b717bd3778785967
stanobilikpa <118492909+stanobilikpa@users.noreply.github.com>
2025-12-14 22:06:13 +0000

    Deploying to gh-pages from @ stanobilikpa/stanobilikpa.github.io@6735ea12d40edb25b93adf750618c5ee77884167 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,8 +407,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+      },{id: "news-i-was-awarded-the-iros-sdc-award-to-support-my-participation-and-presentation-at-ieee-rsj-iros-2025-in-hangzhou-china-recognizing-the-quality-and-impact-of-my-research-in-modular-and-swarm-robotics",
+          title: 'I was awarded the IROS-SDC Award to support my participation and presentation at...',
           description: "",
           section: "News",},{id: "news-i-attended-the-ieee-rsj-international-conference-on-intelligent-robots-and-systems-iros-2025-in-hangzhou-china-from-19th-to-25th-october-2025-during-the-conference-i-presented-our-research-on-scalable-plug-and-play-robotic-fabrics-based-on-kilobot-modules-providing-an-excellent-opportunity-to-share-findings-with-international-robotics-experts-and-engage-in-collaborative-discussions-i-also-served-as-a-social-media-ambassador-promoting-conference-activities-and-research-highlights-across-ieee-ras-platforms",
           title: 'I attended the IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS 2025)...',
